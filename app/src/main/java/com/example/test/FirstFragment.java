2e11a6847a7067260602c114a19f1b45986038e4
Shiro1234orihS <96548862+Shiro1234orihS@users.noreply.github.com>
2024-06-30 10:22:21 +0200

    j'ai fais la page de connection

--- app/src/main/java/com/example/test/FirstFragment.java
+++ app/src/main/java/com/example/test/FirstFragment.java
@@ -28,7 +28,7 @@ private FragmentFirstBinding binding;
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        binding.buttonFirst.setOnClickListener(v ->
+        binding.buttonLogin.setOnClickListener(v ->
                 NavHostFragment.findNavController(FirstFragment.this)
                         .navigate(R.id.action_FirstFragment_to_SecondFragment)
         );
